1b0f4f48e83b86213cd2d5b9ebdc9caf71da696d
Jake Howard <git@theorangeone.net>
2017-07-22 17:02:21 +0100

    Add script to post-process course zip files

---
+++ process.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf zip_dir/
+
+echo "> Unzipping Archive..."
+unzip -qxK Sourcebots_Programming_Tutorial.zip -d zip_dir
+
+cd zip_dir/
+
+echo "> Cleaning out zip..."
+rm -rf env/
+rm -rf test/
+rm -rf .git/
+
+echo "> Moving robot stub into tasks..."
+for task in lesson*/**; do
+    cp -r robot/ $task
+done
+
+echo "> Recompressing..."
+zip -rlqX Sourcebots_Programming_Tutorial_Processed.zip *
+
+cd ../
+
+echo "> Cleaning up..."
+mv zip_dir/Sourcebots_Programming_Tutorial_Processed.zip .
+
+rm -rf zip_dir/
+
+echo "> Done!"
