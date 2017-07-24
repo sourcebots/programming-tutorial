8a41363737ba3d295dd696b46fef0c84c6178cd6
Jake Howard <git@theorangeone.net>
2017-07-24 20:57:09 +0100

    Add servo board stub

--- robot/robot.py
+++ robot/robot.py
@@ -1,7 +1,9 @@
 from robot.motor import MotorBoard
+from robo.servo import ServoBoard
 
 motor0 = MotorBoard('SB123')
 motor1 = MotorBoard('SB456')
+servo0 = ServoBoard('SBABC')
 
 class Robot():
     motor_boards = {
@@ -9,4 +11,9 @@ class Robot():
         'SB456': motor1,
         0: motor0,
         1: motor1
+    }
+
+    servo_boards = {
+        'SBABC': servo0,
+        0: servo0
     }
---
+++ robot/servo.py
@@ -0,0 +1,30 @@
+class Servo():
+    """
+        Stub Servo
+    """
+
+    def __init__(self, board, index):
+        self.board = board
+        self.index = index
+        self._position = 0
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        print("Setting servo {} on {} to {} degrees.".format(self.index, self.board._serial, position))
+        self._position = position
+
+
+class ServoBoard()
+    """
+        Stub Servo Board
+    """
+
+    def __init__(self, serial):
+        self._serial = serial
+        self.ports = {}
+        for i in range(15):
+            self.ports[i] = Servo(i)
