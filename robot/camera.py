66544d6c0aea0d4cb637c60cc3c77041dbcdc6d8
Jake Howard <git@theorangeone.net>
2017-07-26 20:44:03 +0100

    Added vision stubs

---
+++ robot/camera.py
@@ -0,0 +1,42 @@
+from robot.marker import Marker
+
+MARKERS = [
+    {
+        id: 1,
+        polar: (
+            (12, 130, 14),
+            4.5
+        )
+    },
+    {
+        id: 2,
+        polar: (
+            (12, -40, 14),
+            3.2
+        )
+    },
+    {
+        id: 4,
+        polar: (
+            (12, -40, 0),
+            3.2
+        )
+    },
+    {
+        id: 6,
+        polar: (
+            (12, 40, 0),
+            3.2
+        )
+    }
+]
+
+class Camera:
+    def __init__(self, serial):
+        self.serial = serial
+
+    def see(self):
+        print("Looking for markers...")
+        markers = [Marker(**m) for m in MARKERS]
+        print("Found {} markers!".format(len(markers)))
+        return markers
--- robot/robot.py
+++ robot/robot.py
@@ -1,9 +1,14 @@
 from robot.motor import MotorBoard
 from robo.servo import ServoBoard
+from robot.camera import Camera
 
 motor0 = MotorBoard('SB123')
 motor1 = MotorBoard('SB456')
 servo0 = ServoBoard('SBABC')
+camera = Camera('SB789')
+
+def get_first(d):
+    return d[list(d.keys())[0]]
 
 class Robot():
     motor_boards = {
@@ -17,3 +22,12 @@ class Robot():
         'SBABC': servo0,
         0: servo0
     }
+
+    cameras = {
+        'SB789': camera
+    }
+
+    def __init__(self):
+        self.motor_board = get_first(self.motor_boards)
+        self.servo_board = get_first(self.servo_boards)
+        self.camera = get_first(self.cameras)
