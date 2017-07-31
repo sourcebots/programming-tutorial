84593f07d4146ece0fab29ca397b2e04db86f438
Jake Howard <git@theorangeone.net>
2017-07-31 22:49:09 +0100

    Add stubs for GPIO and ultrasound

--- robot/servo.py
+++ robot/servo.py
@@ -1,3 +1,37 @@
+from enum import Enum
+import random
+
+
+class PinMode(Enum):
+    INPUT = 'hi-z'
+    INPUT_PULLUP = 'pullup'
+    OUTPUT_HIGH = 'high'
+    OUTPUT_LOW = 'low'
+
+
+class PinValue(Enum):
+    HIGH = 'high'
+    LOW = 'low'
+
+
+class Gpio():
+    def __init__(self, pin):
+        self._pin = pin
+        self._mode = PinMode.INPUT
+
+    @property
+    def mode(self):
+        return self._pin
+
+    @mode.setter
+    def mode(self, mode):
+        print("Setting GPIO pin {} to {}.".format(self._pin, mode))
+        self._mode = mode
+
+    def read(self):
+        return random.choice([PinValue.HIGH, PinValue.LOW])
+
+
 class Servo():
     """
         Stub Servo
@@ -29,3 +63,10 @@ class ServoBoard():
         self.servos = {}
         for i in range(15):
             self.servos[i] = Servo(self, i)
+
+        self.gpios = {}
+        for i in range(2, 13):
+            self.gpios[i] = Gpio(self, i)
+
+    def read_ultrasound(self, trigger_pin, echo_pin):
+        return random.randint(0, 1000) / 1000
